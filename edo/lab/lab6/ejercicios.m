5d586d31819ddceca09517306b7b1db866c39155
row-d <davidgomez2924@gmail.com>
2023-05-15 12:45:41 -0400

    ejercicio 3 y 5

--- edo/lab/lab6/ejercicios.m
+++ edo/lab/lab6/ejercicios.m
@@ -6,6 +6,7 @@ display('ejercicio 1');
 pretty(sol1);   
 ezplot(sol);
 clear all;
+
 %% ejercicio 2
 syms x(t)
 Dx = diff(x,t)
@@ -15,6 +16,15 @@ display('ejercicio 2');
 pretty(sol);
 ezplot(sol);
 clear all;
+
+%% Ejercicio 3
+syms y(x)
+ED = diff(y) == (2*cos(x).^2 - sin(x).^2 + y^2)/(2*cos(x));
+sol = dsolve(ED);
+display('ejercicio 3');
+pretty(sol);
+clear all;
+
 %% ejercicio 4
 syms y(x)
 Dy = diff(y,x,1);
@@ -24,4 +34,12 @@ sol = dsolve(EDO3,y(1) == 2, Dy(1) == 0);
 display('ejercicio 2');
 pretty(sol);
 ezplot(sol);
-%%
+
+%% Ejercicio 5
+syms y(x)
+Dy = diff(y);
+ED = diff(y,2) - 2*Dy + 3*y == 0;
+sol = dsolve(ED,y(1) == 2,Dy(1) == 0);
+display('ejercicio 3');
+pretty(sol);
+clear all;
